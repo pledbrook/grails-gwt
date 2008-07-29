d9845b69aa603949e34959670d555f92eb64590c
ckchris <ckchris@832c1c66-4827-0410-8465-ccb17913cd09>
2008-07-29 01:14:15 +0000

    - support for module dependency checking and recompiles only modules   that depend on the affected module.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-gwt/branches/ckchris-1.5RC1@42548 832c1c66-4827-0410-8465-ccb17913cd09

--- GwtGrailsPlugin.groovy
+++ GwtGrailsPlugin.groovy
@@ -93,35 +93,32 @@ requests.
 
 	def onChange = {event ->
 		if (event.source instanceof FileSystemResource) {
-			//we need to check if the file is located in a module
-			def moduleInfoMaps = getModuleDef(event.source)
-			if (moduleInfoMaps) {
-				//it exists in a module, so let's specifically compile only these module
-				recompileGwtModule(moduleInfoMaps, event.source)
-			}
+			processResourceForModules(event.source)
 		}
 	}
 
 	def onApplicationChange = {event ->
 	}
 
+	/** finds and loads gwt modules in our classpath    */
 	def loadGwtModules() {
 		if (moduleMap) return
 		print "Loading available GWT modules from the classpath..."
-		def moduleFiles = resolver.getResources("classpath:**/src/gwt/**/*.gwt.xml") as List
-		moduleFiles += resolver.getResources("classpath:**/src/java/**/*.gwt.xml") as List
+		def moduleFileResources = resolver.getResources("classpath:**/src/gwt/**/*.gwt.xml") as List
+		moduleFileResources += resolver.getResources("classpath:**/src/java/**/*.gwt.xml") as List
 		moduleMap = [:]
-		moduleFiles.each {moduleFile ->
-			def path = moduleFile.URI.toString()
+		moduleFileResources.each {moduleFileResource ->
+			def path = moduleFileResource.URI.toString()
 			def m = path =~ /src\/(?:java|gwt)\/([\w\/]+)\.gwt\.xml$/
 			if (m.count > 0) {
 				def moduleName = m[0][1].replace('/' as char, '.' as char)
 				try {
+					LOG.debug "Loading Module: ${moduleName}"
 					def moduleDef = ModuleDefLoader.loadFromClassPath(TreeLogger.NULL, moduleName)
+					def runnable = moduleDef.getEntryPointTypeNames().size() > 0 ? true : false
+					def depModules = getInheritedModules(moduleFileResource.file)
+					moduleMap.put(moduleName, [module: moduleDef, lastCompileTime: 0, runnable: runnable, depModules: depModules])
 					LOG.info "Loaded Module: ${moduleName}"
-					//only store modules with entry points
-					if (moduleDef.getEntryPointTypeNames().size() > 0)
-						moduleMap.put(moduleName, [module: moduleDef, lastCompileTime: 0])
 				} catch (UnableToCompleteException ex) {
 					LOG.warn "Unable to load module ${moduleName}", ex
 				}
@@ -130,73 +127,116 @@ requests.
 		println "Done!"
 	}
 
+	/** parse the gwt file to retrieve the inherited modules    */
+	def getInheritedModules(File modulePath) {
+		def depModules = []
+		modulePath.eachLine {line ->
+			def m = line =~ /<inherits\s+name=["']([\w\.]*)["']/
+			if (m.count > 0) {
+				depModules << m[0][1]
+			}
+		}
+		if (depModules) LOG.debug "Inherits Modules: " + depModules.join(',')
+		return depModules
+	}
+
 	/**
-	 * Checks which gwt this file belongs to.  It will search recursively up the file tree.
-	 * If it can't find a module definition within the classpath, then it will return null.
+	 * Checks which gwt module this file belongs to.
 	 */
-	def getModuleDef(FileSystemResource resource) {
+	def processResourceForModules(FileSystemResource resource) {
 		loadGwtModules()
 		def path = resource.file.absolutePath
 		def m = path =~ /src\/(?:java|gwt)\/(.*)$/
 		if (m.count == 0) return null
 		def normalizedResource = m[0][1]
 
-		def depModuleInfoMaps = []
-		//find public resource
-		for (HashMap moduleInfoMap: moduleMap.values()) {
-			ModuleDef moduleDef = moduleInfoMap.module
-			//this will effectively skip recompiling when multiple files are modified..
-			if (path.endsWith(".java")) {
-				//change our resource into a class notation
-				def className = normalizedResource.replace(File.separatorChar, '.' as char) - ".java"
-				//find source file
-				if (moduleDef.findJavaSourceFile(className) != null) {
-					//skip if our last compile time is already newer than the resource's modification time
-					if (moduleInfoMap.lastCompileTime > resource.lastModified()) continue
-					LOG.debug "Source file used by module ${moduleDef.name}. Setting module for recompile."
-					depModuleInfoMaps << moduleInfoMap
-				}
-			} else {
-				//subtract the module location from the resource
-				def modulePath = moduleDef.name.substring(0, moduleDef.name.lastIndexOf('.')).replace('.' as char, File.separatorChar)
-				def publicResource = normalizedResource - modulePath
-				//ASSUMPTION: public files must all reside in a subdirectory below the module directory
-				publicResource = publicResource.substring(publicResource.indexOf(File.separatorChar as String, 1) + 1)
-				if (moduleDef.findPublicFile(publicResource) != null) {
-					LOG.debug "Public resource used by module ${moduleDef.name}. Copying resource to module directory."
-					//depModuleInfoMaps << moduleInfoMap
-					//since it's a public resource, let's circumvent the recompile process and copy the resource
-					//file directly to the output directory.  This will drastically increase the reload during dev
-					FileUtils.copyFile(resource.file, new File("${outDir}/${moduleDef.name}/${publicResource}"))
+		def affectedModuleInfoMap = null
+		def publicResource = null
+		//ASSUMPTION: one resource belongs to one and only one module
+		if (path.endsWith(".gwt.xml")) {
+			//process gwt.xml by reloading the entire module
+			def fileModuleName = normalizedResource.replace(File.separatorChar, '.' as char) - ".gwt.xml"
+			LOG.debug "gwt.xml file changed for module ${fileModuleName}."
+			affectedModuleInfoMap = moduleMap.get(fileModuleName)
+			//refresh module info
+			affectedModuleInfoMap.module.refresh(TreeLogger.NULL)
+			affectedModuleInfoMap.depModules = getInheritedModules(resource.file)
+		} else {
+			for (HashMap moduleInfoMap: moduleMap.values()) {
+				ModuleDef moduleDef = moduleInfoMap.module
+				if (path.endsWith(".java")) {
+					//change our resource into a class notation
+					def className = normalizedResource.replace(File.separatorChar, '.' as char) - ".java"
+					//find source file
+					if (moduleDef.findJavaSourceFile(className) != null) {
+						//skip if our last compile time is already newer than the resource's modification time
+						if (moduleInfoMap.lastCompileTime < resource.lastModified()) {
+							LOG.debug "Source file used by module ${moduleDef.name}."
+							affectedModuleInfoMap = moduleInfoMap
+						}
+						break
+					}
+				} else {
+					//subtract the module location from the resource
+					def modulePath = moduleDef.name.substring(0, moduleDef.name.lastIndexOf('.')).replace('.' as char, File.separatorChar)
+					publicResource = normalizedResource - modulePath
+					//ASSUMPTION: public files must all reside in a subdirectory below the module directory
+					publicResource = publicResource.substring(publicResource.indexOf(File.separatorChar as String, 1) + 1)
+					if (moduleDef.findPublicFile(publicResource) != null) {
+						LOG.debug "Public resource used by module ${moduleDef.name}."
+						affectedModuleInfoMap = moduleInfoMap
+						break;
+					}
 				}
 			}
 		}
-		return depModuleInfoMaps
+
+		if (affectedModuleInfoMap) {
+			//check which modules inherit this affected module
+			def depModuleInfoMaps = moduleMap.values().findAll {if (it.depModules.contains(affectedModuleInfoMap.module.name)) return true}
+			depModuleInfoMaps << affectedModuleInfoMap
+			LOG.debug "Dependent module info maps: ${depModuleInfoMaps}"
+			//process all dependent modules
+			for (HashMap moduleInfoMap: depModuleInfoMaps) {
+				def moduleDef = moduleInfoMap.module
+				//ASSUMPTION: affected module should not have any dependent modules
+				//only recompile modules that has entry points
+				if (moduleInfoMap.runnable) {
+					if (publicResource) {
+						LOG.debug "Copying public resource ${publicResource} to dependent module ${moduleDef.name}."
+						//since it's a public resource, let's circumvent the recompile process and copy the resource
+						//file directly to the output directory.  This will drastically increase the reload during dev
+						FileUtils.copyFile(resource.file, new File("${outDir}/${moduleDef.name}/${publicResource}"))
+					} else {
+						recompileGwtModule(moduleInfoMap, resource)
+					}
+				}
+			}
+		}
 	}
 
 	/**
 	 * Runs the GWTCompiler to recompile the module
 	 */
-	def recompileGwtModule(moduleInfoMaps, resource) {
-		moduleInfoMaps.each {moduleInfoMap ->
-			def module = moduleInfoMap.module
-			print "Recompiling GWT module ${module.name}..."
-			try {
-				//refresh will update new/removed/modified/stale files
-				module.refresh(TreeLogger.NULL)
-				GWTCompiler compiler = new GWTCompiler()
-				compiler.setStyleObfuscated()
-				compiler.setOutDir(new File(outDir))
-				compiler.setModuleName(module.name)
-				compiler.distill(TreeLogger.NULL, module)
-				println "Done!"
-				moduleInfoMap.lastCompileTime = System.currentTimeMillis()
-			} catch (InterruptedException ex) {
-				LOG.warn "Interrupted while waiting for compiler to finish"
-			} catch (UnableToCompleteException ex) {
-				println "ERROR!"
-				LOG.error "Unable to recompile module ${module.name}", ex
-			}
+	def recompileGwtModule(HashMap moduleInfoMap, FileSystemResource resource) {
+		def module = moduleInfoMap.module
+		if (moduleInfoMap.lastCompileTime >= resource.lastModified()) return
+		print "Recompiling GWT module ${module.name}..."
+		try {
+			//refresh will update new/removed/modified/stale files
+			module.refresh(TreeLogger.NULL)
+			GWTCompiler compiler = new GWTCompiler()
+			compiler.setStyleObfuscated()
+			compiler.setOutDir(new File(outDir))
+			compiler.setModuleName(module.name)
+			compiler.distill(TreeLogger.NULL, module)
+			println "Done!"
+			moduleInfoMap.lastCompileTime = System.currentTimeMillis()
+		} catch (InterruptedException ex) {
+			LOG.warn "Interrupted while waiting for compiler to finish"
+		} catch (UnableToCompleteException ex) {
+			println "ERROR!"
+			LOG.error "Unable to recompile module ${module.name}", ex
 		}
 	}
 }
